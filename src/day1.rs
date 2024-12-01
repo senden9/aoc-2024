a1da668ebe87388d396781337cf7145855b1f27c
Stefano Probst <senden9@gmail.com>
2024-12-01 16:29:45 +0100

    Add more tests to day 1

--- src/day1.rs
+++ src/day1.rs
@@ -8,9 +8,9 @@ pub fn input_generator(input: &str) -> (Vec<u32>, Vec<u32>) {
     let mut numbers2 = Vec::<u32>::with_capacity(nr_lines);
 
     for line in input.lines() {
-        let mut line_iter = line.split_ascii_whitespace();
-        numbers1.push(line_iter.next().unwrap().parse::<u32>().unwrap());
-        numbers2.push(line_iter.next().unwrap().parse::<u32>().unwrap());
+        let line_iter = line.split_once("   ").unwrap();
+        numbers1.push(line_iter.0.parse::<u32>().unwrap());
+        numbers2.push(line_iter.1.parse::<u32>().unwrap());
     }
     (numbers1, numbers2)
 }
@@ -51,28 +51,45 @@ pub fn part2(input: &(Vec<u32>, Vec<u32>)) -> u32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
 
-    #[test]
-    fn part1_example() {
-        let ex_input = r"3   4
+    const EXAMPLE_INPUT: &str = r"3   4
 4   3
 2   5
 1   3
 3   9
 3   3";
-        let input_parsed = input_generator(ex_input);
+
+    #[test]
+    fn part1_example() {
+        let input_parsed = input_generator(EXAMPLE_INPUT);
         assert_eq!(part1(&input_parsed), 11);
     }
 
     #[test]
     fn part2_example() {
-        let ex_input = r"3   4
-4   3
-2   5
-1   3
-3   9
-3   3";
-        let input_parsed = input_generator(ex_input);
+        let input_parsed = input_generator(EXAMPLE_INPUT);
         assert_eq!(part2(&input_parsed), 31);
+    }
+
+    fn read_input(file_name: &str) -> String {
+        fs::read_to_string(file_name)
+            .expect("Unable to read input!")
+            .trim()
+            .to_string()
+    }
+
+    #[test]
+    fn part1_input() {
+        let input = read_input("input/2024/day1.txt");
+        let s = input_generator(&input);
+        assert_eq!(part1(&s), 2769675);
+    }
+
+    #[test]
+    fn part2_input() {
+        let input = read_input("input/2024/day1.txt");
+        let s = input_generator(&input);
+        assert_eq!(part2(&s), 24643097);
     }
 }
